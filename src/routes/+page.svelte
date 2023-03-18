e7f7a3f116ae44bbdb3f5e8dce2e67ca85d3f96e
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-03-18 14:33:29 -0400

    test environment variables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { PUBLIC_PROXY_ENDPOINT } from "$env/static/public";
+</script>
+
+<h1>The proxy endpoint is {PUBLIC_PROXY_ENDPOINT}</h1>
